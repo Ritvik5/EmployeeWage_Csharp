dd1a4a5cbacc5b321f9643946dd61ced8b7fb57a
Ritvik5 <sharma.ritvik56@gmail.com>
2023-02-16 16:36:14 +0530

    Part Time Wage

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Employee
+﻿using System.Xml;
+
+namespace Employee
 {
     internal class Program
     {
@@ -6,23 +8,28 @@
         {
             //Constatnts
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             //Variables
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
+            Random random= new Random();
             //Computation
-            int empCheck = random.Next(0,2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0,3);
+            if (empCheck == IS_FULL_TIME) 
             {
                 empHrs = 8;
             }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
             else
             {
-                empHrs = 0;    
+                empHrs= 0;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage : " + empWage);
+            Console.WriteLine("Emp wage is:"+empWage);
         }
     }
 }

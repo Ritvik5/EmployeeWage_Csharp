5c7c75580e1641a50f56837eed237ef3a39578fd
Ritvik5 <sharma.ritvik56@gmail.com>
2023-02-16 17:06:59 +0530

    Calculating wage till a condition reached

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -6,15 +6,20 @@ namespace Employee
     {
         static void Main(string[] args)
         {
+            //Constants
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
-            int NUM_OF_WORKING_DAYS = 20;
+            const int EMP_RATE_PER_HOUR = 20;
+            const int NUM_OF_WORKING_DAYS = 20;
+            const int MAX_HRS_IN_MONTH = 10;
+            //Variable
             int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            //Computation
+            while ( totalEmpHrs <= MAX_HRS_IN_MONTH &&  totalWorkingDays <  NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -29,10 +34,10 @@ namespace Employee
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Emp Wage : " + empWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#: " + totalWorkingDays + "Emp Hrs :" +empHrs);
             }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Emp Wage : " + totalEmpWage);
         }
     }

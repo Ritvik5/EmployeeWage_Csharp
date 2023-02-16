7722516bfe7c3f6eebd94e3e76109c7f35fd1403
Ritvik5 <sharma.ritvik56@gmail.com>
2023-02-16 16:50:09 +0530

    Total Wage

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -6,30 +6,34 @@ namespace Employee
     {
         static void Main(string[] args)
         {
-            //Constatnts
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
-            const int EMP_RATE_PER_HOUR = 20;
-            //Variables
+            int EMP_RATE_PER_HOUR = 20;
+            int NUM_OF_WORKING_DAYS = 20;
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
-            //Computation
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int totalEmpWage = 0;
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage : " + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage : " + empWage);
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
         }
     }
 }

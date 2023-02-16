7bd0d62c8f7eca0313318dced1e3abe1fefdd4c7
Ritvik5 <sharma.ritvik56@gmail.com>
2023-02-16 16:46:24 +0530

    Using Switch Case

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -7,29 +7,29 @@ namespace Employee
         static void Main(string[] args)
         {
             //Constatnts
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
+            const int EMP_RATE_PER_HOUR = 20;
             //Variables
             int empHrs = 0;
             int empWage = 0;
-            Random random= new Random();
+            Random random = new Random();
             //Computation
-            int empCheck = random.Next(0,3);
-            if (empCheck == IS_FULL_TIME) 
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
             {
-                empHrs = 8;
-            }
-            else if (empCheck == IS_PART_TIME)
-            {
-                empHrs = 4;
-            }
-            else
-            {
-                empHrs= 0;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp wage is:"+empWage);
+            Console.WriteLine("Emp Wage : " + empWage);
         }
     }
 }

e722ed63ee64fdf3fb6207d9c9d1111c9af4f584
Ritvik5 <sharma.ritvik56@gmail.com>
2023-02-21 18:14:21 +0530

    Using ComputeEmpWage Function Into Main

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -1,21 +1,28 @@
-﻿using System.Xml;
+﻿using System.Data;
+using System.Xml;
 
 namespace Employee
 {
-    internal class Program
+    
+    
+    public class Program
     {
-        static void Main(string[] args)
+        //Constants
+        const int IS_FULL_TIME = 1;
+        const int IS_PART_TIME = 2;
+        const int EMP_RATE_PER_HOUR = 20;
+        const int NUM_OF_WORKING_DAYS = 20;
+        const int MAX_HRS_IN_MONTH = 100;
+
+        //Create Function
+        public static int ComputeEmpWage()
         {
-            //Constants
-            const int IS_FULL_TIME = 1;
-            const int IS_PART_TIME = 2;
-            const int EMP_RATE_PER_HOUR = 20;
-            const int NUM_OF_WORKING_DAYS = 20;
-            const int MAX_HRS_IN_MONTH = 10;
+            
             //Variable
             int empHrs = 0;
             int totalEmpHrs = 0;
             int totalWorkingDays = 0;
+
             //Computation
             while ( totalEmpHrs <= MAX_HRS_IN_MONTH &&  totalWorkingDays <  NUM_OF_WORKING_DAYS)
             {
@@ -39,6 +46,13 @@ namespace Employee
             }
             int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+            return totalEmpWage;
+        }
+
+        public static void Main(string[] args)
+        {
+            //Calling Function into Main Method
+            ComputeEmpWage();
         }
     }
 }
